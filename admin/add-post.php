a46f221283d2043fba0f72a5e2405713f619799a
waqarmughal98 <waqarmughal005@gmail.com>
2021-03-01 16:35:31 +0500

    1 March Changes

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,16 +1,12 @@
 <?php
-    require_once('assets/attach/top.php');
-    // Run Queries
-    $queries = new Queries;
+    include('assets/attach/top.php');
+    ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 ?>
-	<!-- Bootstrap Dropify CSS -->
-	<!-- <link href="../vendors/bower_components/dropify/dist/css/dropify.min.css" rel="stylesheet" type="text/css"/> -->
-	<link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
 	</head>
 	<body>
-		<!--sidebar-->
+		<!--Preloader-->
 <?php
-	require_once('assets/attach/sidebar.php');
+    include('assets/attach/sidebar.php');
 ?>
 			
 			<!-- Main Content -->
@@ -19,7 +15,7 @@
 					<!-- Title -->
 					<div class="row heading-bg">
 						<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-						  <h5 class="txt-dark">Add-Posts</h5>
+						  <h5 class="txt-dark">Add-Post</h5>
 						</div>
 						<!-- Breadcrumb -->
 						<div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
@@ -40,41 +36,42 @@
 								<div class="panel-wrapper collapse in">
 									<div class="panel-body">
 										<div class="form-wrap">
-											<form action="" method="POST">
+											<form action="" method="post" enctype="multipart/form-data"> 
 												<h6 class="txt-dark capitalize-font"><i class="zmdi zmdi-info-outline mr-10"></i>about post</h6>
 												<hr class="light-grey-hr"/>
 												<div class="row">
 													<div class="col-md-6">
 														<div class="form-group">
 															<label class="control-label mb-10">Title</label>
-															<input type="text" id="firstName" name="title" class="form-control" placeholder="Enter your title" required>
+															<input type="text" id="title" name="title" class="form-control" placeholder="Enter your title">
 														</div>
 													</div>
 													<!--/span-->
 													<div class="col-md-6">
 														<div class="form-group">
 															<label class="control-label mb-10">Meta title</label>
-															<input type="text" id="lastName" name="meta_title" class="form-control" placeholder="Enter meta title" required>
+															<input type="text" id="meta_title" name="meta_title" class="form-control" placeholder="Enter your Meta title">
 														</div>
 													</div>
 													<!--/span-->
 												</div>
-												<!-- Row -->
-                                                <div class="row">
-                                                    <div class="col-md-12">
+												<div class="row">
+													<!--/span-->
+													<div class="col-md-12">
 														<div class="form-group">
 															<label class="control-label mb-10">Meta description</label>
-                                                            <textarea class="form-control" name="meta_description" rows="2" cols="80" placeholder="Meta Description" required></textarea>
+															<input type="text" id="meta_description" name="meta_description" class="form-control" placeholder="Enter your Meta description">
 														</div>
 													</div>
-                                                </div>
-												<!-- Row -->
+													<!--/span-->
+												</div>
+                                                <!-- Row -->
 												<h6 class="txt-dark capitalize-font mt-15"><i class="zmdi zmdi-comment-text mr-10"></i>Post Description</h6>
 												<hr class="light-grey-hr"/>
 												<div class="row">
 													<div class="col-md-12">
 														<div class="form-group">
-															<textarea name="description" class="form-control tinymce" required></textarea>
+                                                            <textarea name="description" id="editor1" rows="10" cols="80"></textarea>
 														</div>
 													</div>
 												</div>
@@ -82,9 +79,9 @@
 												<div class="row">
 													<div class="col-md-6">
 														<div class="form-group">
-															<label class="control-label mt-10 mb-10">Category</label>
-															<select name="category" class="form-control" data-placeholder="Choose a Category">
-																<option value="" selected disabled>Please Select Category</option>
+															<label class="control-label mb-10">Category</label>
+															<select class="form-control" data-placeholder="Choose a Category" name="category">
+                                                                <option value="" selected disabled>Please Select Category</option>
 																<?php
 																$selcat = "SELECT * FROM `categories` WHERE domain='$domain'";
 																$queries->query($selcat);
@@ -104,7 +101,7 @@
 													<!--/span-->
 													<div class="col-md-6">
 														<div class="form-group">
-															<label class="control-label mt-10 mb-10">Status</label>
+															<label class="control-label mb-10">Status</label>
 															<div class="radio-list">
 																<div class="radio-inline pl-0">
 																	<div class="radio radio-info">
@@ -125,10 +122,10 @@
 												</div>
 												<!--/row-->
 												<div class="row">
-													<div class="col-md-6 mt-10">
+                                                <div class="col-md-6 mt-10">
 														<div class="form-group">
 															<label class="control-label mb-10" required>Tags <span class="text-danger">[Please Add (,) comma sign between tags]</span></label>
-															<input name="tags" type="text" class="form-control">
+															<input name="tags" type="text" name="tags" class="form-control">
 														</div>
 													</div>
 													<!--/span-->
@@ -138,22 +135,25 @@
 															<input name="seo_url" type="text" class="form-control">
 														</div>
 													</div>
+													<!--/span-->
 												</div>
-												<div class="seprator-block"></div>
+												<!--/row-->
 												<h6 class="txt-dark capitalize-font"><i class="zmdi zmdi-collection-image mr-10"></i>upload image</h6>
 												<hr class="light-grey-hr"/>
 												<div class="row">
 													<div class="col-lg-12">
 														<div class="img-upload-wrap">
-															<input type="file" name="featured" class="filepond">
+															<img class="img-responsive" src="../img/chair.jpg" alt="upload_img"> 
+														</div>
+														<div class="fileupload btn btn-info btn-anim"><i class="fa fa-upload"></i><span class="btn-text">Upload new image</span>
+															<input type="file" class="upload" name="featured">
 														</div>
 													</div>
 												</div>
+                                                <div class="seprator-block"></div>
 												<hr class="light-grey-hr"/>
-
 												<div class="form-actions">
-													<button class="btn btn-success btn-icon left-icon mr-10 pull-left" name="publish"> <i class="fa fa-check"></i> <span>Publish</span></button>
-													<!-- <button type="button" class="btn btn-warning pull-left">Cancel</button> -->
+													<button class="btn btn-success" type="submit" name="publish"> <i class="fa fa-check"></i> <span>Publish</span></button>
 													<div class="clearfix"></div>
 												</div>
 											</form>
@@ -164,7 +164,8 @@
 						</div>
 					</div>
 					<!-- /Row -->
-					<?php
+                    
+                    <?php
 						if(isset($_POST['publish'])):
 							$title = htmlentities($_POST['title']);
 							$meta_title = htmlentities($_POST['meta_title']);
@@ -202,8 +203,8 @@
 							$date = date("l-d-m-y");
 
 							// Featured Image							
-                            $uploaddir = "../cryptoinject/images/";
-                            $image = rand().'.'.pathinfo($_FILES['featured']['name'],PATHINFO_EXTENSION);
+                            $uploaddir = "/admin/uploads/";
+                            $  = rand().'.'.pathinfo($_FILES['featured']['name'],PATHINFO_EXTENSION);
                             move_uploaded_file($_FILES["featured"]["tmp_name"],$uploaddir . $image);
 
 							$insertBlog = "INSERT INTO posts (`title`, `description`, `meta_title`, `meta_description`, `category_id`, `sub_category_id`, `image`, `seo_url`, `date`, `domain`, `tags`) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
@@ -214,31 +215,28 @@
 							echo("<script> window.open('add-post.php','_self'); </script>");
 						endif;
 					?>
+
 				</div>
-				
-				<!-- Footer -->
 <!-- jQuery -->
 <script src="../vendors/bower_components/jquery/dist/jquery.min.js"></script>
 
-<script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="../vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
+<script src="https://cdn.ckeditor.com/4.13.1/full/ckeditor.js"></script>
 <script>
-	FilePond.parse(document.body);
+    CKEDITOR.replace( 'editor1' );
+    CKEDITOR.replace( 'editor2' );
+    CKEDITOR.replace( 'editor3' );
+    CKEDITOR.replace( 'editor4' );
+    CKEDITOR.replace( 'editor5' );
+    CKEDITOR.replace( 'editor1', {
+        extraPlugins: 'sourcedialog',
+        removePlugins: 'sourcearea',
+        removePlugins: 'htmlwriter'
+    });
 </script>
-
-<!-- Bootstrap Daterangepicker JavaScript -->
-<!-- <script src="../vendors/bower_components/dropify/dist/js/dropify.min.js"></script> -->
-
-<!-- Form Flie Upload Data JavaScript -->
-<!-- <script src="dist/js/form-file-upload-data.js"></script> -->
-
-<!-- Tinymce JavaScript -->
-<script src="../vendors/bower_components/tinymce/tinymce.min.js"></script>
-			
-<!-- Tinymce Wysuhtml5 Init JavaScript -->
-<script src="dist/js/tinymce-data.js"></script>
-
-
+<!-- Footer -->
 <?php
-    require_once('admin/assets/footer.php');
+    include('assets/attach/footer.php');
 ?>
