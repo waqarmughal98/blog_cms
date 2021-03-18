4b16ed1c5cc5db6524c3452ff1577071316b30a8
waqarmughal98 <waqarmughal005@gmail.com>
2021-03-18 16:33:24 +0500

    Changes

--- attach/footer.php
+++ attach/footer.php
@@ -9,11 +9,12 @@
 							<div class="blog_followus_div">
 								<ul>
 									<li>Follow Us -</li>
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+									<li><a href="https://www.facebook.com/crypto.inject"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="https://twitter.com/CryptoInject"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="https://www.youtube.com/channel/UCcTJFRsLQe0k7-JVFt1IHkQ"><i class="fa fa-youtube-play"></i></a></li>
+									<li><a href="https://www.instagram.com/crypto_inject/"><i class="fa fa-instagram"></i></a></li>
+									<li><a href="https://www.pinterest.it/cryptoinject/_saved/"><i class="fa fa-pinterest"></i></a></li>
+									<li><a href="#"><i class="fa fa-telegram"></i></a></li>
 								</ul>
 							</div>
 						</div>
