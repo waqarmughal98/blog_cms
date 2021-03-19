f5bfdce548eb683f9c330e6a9934e943a3b90f3a
waqarmughal98 <waqarmughal005@gmail.com>
2021-03-19 16:12:45 +0500

    Chnages

---
+++ error.php
@@ -0,0 +1,39 @@
+<?php
+    require_once('attach/top.php');
+?>
+</head>
+<body class="homepage2">
+<?php
+    require_once('attach/header.php');
+?>
+	<div class="blog_breadcrumb_wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="blog_breadcrumb_div">
+						<h3>Error 404</h3>
+						<ol class="breadcrumb">
+							<li>You are here:</li>
+							<li><a href="index">Home</a></li>
+							<li>Error 404</li>
+						</ol>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="blog_main_wrapper blog_toppadder100 blog_bottompadder100">
+		<div class="blog_innerpages blog_aboutpage">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-12">
+				<div class="blog_error_data">
+					<h1>Error</h1>
+					<h2>404</h2>
+					<h3>Ooops!! page Not Found !</h3>
+					<a href="index.html" class="blog_btn blog_bg_pink">back to home</a>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+    require_once('attach/footer.php');
+?>
